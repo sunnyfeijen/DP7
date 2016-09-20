83554a512a8b8328c557dce3bfd42a9731423970
sunnyfeijen <Sunny@MacBook-Pro-van-Sunny.local>
2016-09-20 11:33:33 +0200

    update master

--- MASTER/MASTER.ino
+++ MASTER/MASTER.ino
@@ -1,29 +1,36 @@
+#include <Wire.h>
 #include <NewPing.h>
 
 #define TRIGGER_PIN 12
 #define ECHO_PIN 11
-#define MAX_DISTANCE 300
+#define MAX_DISTANCE 350
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
+int x = 0;
 int val = 0;
 int prev;
 
 void setup() {
-
-  Serial.begin(9600);           // start serial for output
+  Wire.begin();
+  Serial.begin(9600);
 }
 
 void loop() {
-
+  x = 0;
   prev = val;
 
-  unsigned int uS = sonar.ping(); 
+  unsigned int uS = sonar.ping();
   val = uS / US_ROUNDTRIP_CM;
-
-  if(prev - val > 20){
-    Serial.println("closer"); 
+  Serial.println(val);
+  if(prev - val > 30){
+    x = 1;
+    Serial.println("closer");
+    Wire.beginTransmission(8);
+    Wire.write(x);
+    Wire.endTransmission();
+    delay(5000);
   }
   
-  delay(1000);   
+  delay(50);
 }
